a4a927d48393ab364f6c551ab6a399a23223ec2f
Shulamit Ajzner <shulajz@hotmail.com>
2018-10-22 12:20:00 +0300

    flicker red blue

---
+++ ex1/buttonActions.js
@@ -0,0 +1,10 @@
+function changeColor() {
+
+  if(document.getElementById("demo").style.backgroundColor == "blue")
+   {
+    document.getElementById("demo").style.backgroundColor = "red";
+  }
+  else {
+    document.getElementById("demo").style.backgroundColor = "blue";
+  }
+}
