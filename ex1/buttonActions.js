22ac7de590e994fcb0e8693f497c1e8ae7cb4db2
Shulamit Ajzner <shulajz@hotmail.com>
2018-10-25 21:48:21 +0300

    array of colors

--- ex1/buttonActions.js
+++ ex1/buttonActions.js
@@ -1,10 +1,13 @@
-function changeColor() {
 
-  if(document.getElementById("demo").style.backgroundColor == "blue")
-   {
-    document.getElementById("demo").style.backgroundColor = "red";
-  }
-  else {
-    document.getElementById("demo").style.backgroundColor = "blue";
-  }
+var i = 1;
+function changeColor() {
+  var colorArray = ['#FF6633', '#CC9999','#4D80CC'];
+    //store
+    sessionStorage.setItem("color", colorArray[i]);
+    // Retrieve
+    document.getElementById("demo").style.backgroundColor = sessionStorage.getItem("color");
+    i++;
+    if(i == colorArray.length){
+      i = 0;
+    }
 }
